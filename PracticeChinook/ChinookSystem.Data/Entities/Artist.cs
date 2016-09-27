0646df08db6b43a8e684bd58dbd35cfae8d39f04
Don Welch <dwelch@nait.ca>
2016-09-26 21:11:36 -0600

    Entity review and creation

---
+++ PracticeChinook/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    //[Table("sqltablename")]
+    [Table("Artists")]
+    public class Artist
+    {
+        //pkey annotation is optional
+        //default assumes identity sql table
+        //default assumes property name ends with ID or Id
+        //   and sql pkey ends the same
+        //otherwise [Key] is need
+        //non-identity pkeys, compound pkeys, pkeys not
+        // ending in ID or Id
+
+        //class properties map to sql table attributes
+        //properties should be named the same as table attributes
+        //properties should be in the same order as
+        //    table attributes for ease of maintenance
+        [Key]
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
