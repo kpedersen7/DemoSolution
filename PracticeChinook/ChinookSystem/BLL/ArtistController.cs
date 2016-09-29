8cb8b2d1bf7e1ed25ef32d34992d77b793b36a0e
Don Welch <dwelch@nait.ca>
2016-09-28 20:55:52 -0600

    BLL and Web display of entity via EntityFramework

---
+++ PracticeChinook/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.Data.Entities; //entity classes
+using ChinookSystem.DAL;            //context class
+using System.ComponentModel;        //ODS
+#endregion
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class ArtistController
+    {
+        //dump the entire artist table
+        //this will use EntityFramework access
+        //Entity classes will be used to define the data
+
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<Artist> Artist_ListAll()
+        {
+            //set up the transaction area
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.ToList();
+            }
+        }
+    }
+}
