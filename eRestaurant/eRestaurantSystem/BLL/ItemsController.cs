9a57911d7ba34d580735dc3264c9ff379c457666
Don Welch <dwelch@nait.ca>
2016-10-05 20:10:21 -0600

    set up DAL and db connections

---
+++ eRestaurant/eRestaurantSystem/BLL/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel;
+using eRestaurantSystem.DAL;
+using eRestaurantSystem.Data.Entities;
+#endregion
+
+namespace eRestaurantSystem.BLL
+{
+    [DataObject]
+    public class ItemsController
+    {
+    }
+}
