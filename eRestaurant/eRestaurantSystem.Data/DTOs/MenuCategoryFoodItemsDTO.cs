8fc5b09ad98b96abb74978d46676fb1021c61d54
Don Welch <dwelch@nait.ca>
2016-10-05 20:51:54 -0600

    create DTO and POCO classes for queries

---
+++ eRestaurant/eRestaurantSystem.Data/DTOs/MenuCategoryFoodItemsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eRestaurantSystem.Data.POCOs;
+#endregion
+namespace eRestaurantSystem.Data.DTOs
+{
+    public class MenuCategoryFoodItemsDTO
+    {
+        public string MenuCategoryDescription { get; set; }
+        public List<FoodItemCounts> FoodItems { get; set; } 
+    }
+}
