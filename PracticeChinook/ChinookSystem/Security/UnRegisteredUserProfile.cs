74e5db74eabcaddc4e8b1fc5ef54dece73a12768
Don Welch <dwelch@nait.ca>
2016-10-17 20:48:46 -0600

    setup of skeleton bootstrap tabs and tab profiles
    create a new page with skeleton bootstrap tabs
    create tab profiles

---
+++ PracticeChinook/ChinookSystem/Security/UnRegisteredUserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Security
+{
+    public enum UnregisteredUserType { Undefined, Employee, Customer};
+    public class UnRegisteredUserProfile
+    {
+        public int id { get; set; }
+        public UnregisteredUserType UserType { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string AssignedUserName { get; set; }
+        public string AssignedEmail { get; set; }
+    }
+}
